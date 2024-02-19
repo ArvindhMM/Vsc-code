4ac978d1e9e94357bf93bf1fe1bec3a0df94f7de
ArvindhMM <139652026+ArvindhMM@users.noreply.github.com>
2024-02-19 12:25:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const headingElement = document.getElementById('heading')
+const buttonElement = documnet.getElementById('styleButton')
+
+function changeColor() {
+    headingElement.style.color = 'green'
+}
+
